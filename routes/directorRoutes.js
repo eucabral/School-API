85bc4fa721a7cbb308ba8a12aad87d4041a5a751
eucabral <101058486+eucabral@users.noreply.github.com>
2022-08-12 13:21:06 -0300

    example commit

--- routes/directorRoutes.js
+++ routes/directorRoutes.js
@@ -5,5 +5,6 @@ const directorController = require('../controllers/directorController')
 
 router.post('/',directorController.Register)
 router.post('/',directorController.login)
+// router.post('/',directorController.login)
 
 module.exports = router
